f0843bdd3b6e26e5c39d6490f4a15309c3c5910a
quentinrld <71373028+quentinrld@users.noreply.github.com>
2020-12-12 14:07:31 +0100

    Update projetPerso.js

--- js/projetPerso.js
+++ js/projetPerso.js
@@ -2,17 +2,16 @@
 let nouvelActivite = "";
 let tempsActivite = 0;
 let tempsTot = 0;
+let activite = [];
 function ajouterActivite(){
     nouvelActivite = document.ajouterSport.nonActivite.value;
     tempsActivite = Number(document.ajouterSport.tempsActivite.value);
     tempsTot += tempsActivite;
     document.affichageSport.activiteAjoute.value = nouvelActivite;
     document.affichageSport.tempsTotalActivite.value = tempsTot;
-    let activite = new Object();
-    activite.name = nouvelActivite;
-    activite.time = tempsActivite
-    document.affichageSport.toutesActivites.value = activite
-
+    activite.push({nouvelActivite : tempsActivite});
+    var nouvel = "<option>nouvelActivite"+" pendant "+"tempsActivite</option>"
+    document.getElementById("activite").innerHTML = nouvel;
 }
 function reset(){
     nouvelActivite = "réinitialisé";
@@ -21,3 +20,4 @@ function reset(){
     document.affichageSport.tempsTotalActivite.value = tempsTot;
     document.affichageSport.toutesActivites.value = "réinitialisé"
 }
+
