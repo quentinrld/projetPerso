9e748e8429261109e2008486b56dcebf4fc2d387
quentinrld <71373028+quentinrld@users.noreply.github.com>
2020-12-13 19:35:01 +0100

    Update projetPerso.js

--- js/projetPerso.js
+++ js/projetPerso.js
@@ -2,22 +2,49 @@
 let nouvelActivite = "";
 let tempsActivite = 0;
 let tempsTot = 0;
-let activite = [];
+let activite = {};
+
+function initialiserPage(){
+
+    var table = "calculateur de votre temps d'activitée physique";
+    document.getElementById("sport").innerHTML = table;
+    alert("Bienvenu dans cette aplication que permet de calculer le nombre d'heure de sport effectuer");
+}
+/**
+ * cette fonction permet de recupérer les valeurs des formulairs, de  les enregisstrer et de les ecrirer dans de nouveaux formulairs
+ * @param{text} nom de l'activité enregistrer
+ * @param{number} temps de pratique de cette activité
+ * @return{text} nom de la dernière activitée
+ * @return{number} le nombre totale d'heure d'activité
+ * @return{list} la lliste de toute les activitée
+ */
 function ajouterActivite(){
-    nouvelActivite = document.ajouterSport.nonActivite.value;
+    nouvelActivite = document.ajouterSport.nomActivite.value;
     tempsActivite = Number(document.ajouterSport.tempsActivite.value);
     tempsTot += tempsActivite;
+    afficherActivite()
+}
+
+function afficherActivite(){
     document.affichageSport.activiteAjoute.value = nouvelActivite;
     document.affichageSport.tempsTotalActivite.value = tempsTot;
-    activite.push({nouvelActivite : tempsActivite});
-    var nouvel = "<option>nouvelActivite"+" pendant "+"tempsActivite</option>"
+    activite[nouvelActivite] = new Date().toLocaleString('fr-BE');
+    let nouvel = "";
+    for(let i in Object.keys(activite)) {
+        nouvel += "<option>"+Object.keys(activite)[i]+ " pendant " +tempsActivite+" heure</option>";
+    }
     document.getElementById("activite").innerHTML = nouvel;
 }
+/**
+ * cette fonction permet de réinitialiser l'objet des activités
+ * @param {array} activite
+ * @return {array} activite
+ */
 function reset(){
     nouvelActivite = "réinitialisé";
     tempsTot = 0;
     document.affichageSport.activiteAjoute.value = nouvelActivite;
     document.affichageSport.tempsTotalActivite.value = tempsTot;
-    document.affichageSport.toutesActivites.value = "réinitialisé"
+    document.affichageSport.touteActivite.value = "réinitialisé";
+    activite = {};
 }
-
